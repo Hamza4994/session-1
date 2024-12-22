ed5c06e57ea542039738f2d833436d684ca9eaa3
Hamza4900 <haskrall4934@gmail.com>
2024-12-22 15:22:35 +0300

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/Hamza4900/session"
+                text="admin", url=""
             ),
         ],
     ]
