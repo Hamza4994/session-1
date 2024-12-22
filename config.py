93f3473eb3ec81f2d2dae70f3fbea594afb40b8d
Hamza4900 <haskrall4934@gmail.com>
2024-12-22 15:19:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID","23272158"))
+API_HASH = getenv("API_HASH","9c6b7311bf4b00c649674db048c52cbf")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN","7122702201:AAEH-RPL5Huem69X_c_fOgpXlg3YT7Ukr08")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Kingbrukh:kingkhan@kingbruh.ra3pjgm.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 7583810003))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/konnusanlar")
